3475358fddfcc9dfae4c4bfae4820fd64abf2b9b
cdtinney <colintinney@gmail.com>
2024-07-20 15:06:14 -0700

    use prod env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  apps : [{
-    name   : "preset-proxy",
-    script : "./index.js"
+  apps: [{
+    name: "preset-proxy",
+    script: "./index.js",
+    env: {
+      "NODE_ENV": "production"
+    }
   }]
 }
