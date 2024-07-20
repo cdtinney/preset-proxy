6e43025dced876081952ae613b588a8adb4414a4
cdtinney <colintinney@gmail.com>
2024-07-20 14:09:37 -0700

    use request ID

--- index.ts
+++ index.ts
@@ -4,13 +4,23 @@ const { createServer } = require('node:http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const testPresetUrl = `https://pvme.io/preset-maker/#/TR1NQ3YaNPGQQ8mOgzCh`;
+const basePresetUrl = `https://pvme.io/preset-maker/#/`;
 
-const getPresetAsImage = async () => {
+const cachedImages: Record<string, any> = {};
+
+const getPresetAsImage = async (presetId: string) => {
+  if (cachedImages[presetId]) {
+    console.info(`Using cached image for preset: ${presetId}`);
+    return cachedImages[presetId];
+
+  }
+
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   page.setViewport({ width: 1200, height: 700 });
-  await page.goto(testPresetUrl);
+
+  const presetUrl = `${basePresetUrl}${presetId}`;
+  await page.goto(presetUrl);
 
   // Wait for API requests to finish
   await page.waitForNetworkIdle();
@@ -18,7 +28,13 @@ const getPresetAsImage = async () => {
   try {
     const presetSelector = '.preset-editor__export-container';
     const presetDom = await page.$(presetSelector);
+    if (!presetDom) {
+      return;
+    }
+
     const imageBuffer = await presetDom.screenshot({ omitBackground: true });
+    // test without caching
+    // cachedImages[presetId] = imageBuffer;
     return imageBuffer;
   } finally {
     await page.close();
@@ -27,9 +43,16 @@ const getPresetAsImage = async () => {
 };
 
 const server = createServer(async (req: any, res: any) => {
-  console.error(req.params);
-  console.error(req.query.id);
-  const image = await getPresetAsImage();
+  console.info(`Proxy request: ${req.url}`);
+
+  const url = new URL(`http://${process.env.HOST ?? 'localhost'}${req.url}`); 
+  const presetId = url.searchParams.get('id');
+  // Ignore all other requests
+  if (!presetId) {
+    return;
+  }
+
+  const image = await getPresetAsImage(presetId);
   
   res.statusCode = 200;
   res.setHeader('Content-Type', `image/jpeg`);
